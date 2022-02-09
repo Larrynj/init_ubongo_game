b79da5c9a9d02476c24f4c0a6f015be3599c53d0
Renegade970 <72569921+Larrynj@users.noreply.github.com>
2022-02-09 20:57:30 +0100

    Updated .py

--- ubongo_game.py
+++ ubongo_game.py
@@ -40,17 +40,6 @@ print(default_board,"\n")
 # paste(default_board, pieceRed, (0,0))
 # print(default_board,"\n")
 
-'''
-The algorithm should work as follows,
-- A first piece is selected from order_1 and inserted in the playing board at the starting coord_(0,0) og_board = temp_board.
-- A second piece from order_2 is taken and added to this coord_ as well(temp_board is refreshed).
-- If the sum of this addition on the board results in a 3 or the missing piece of the board is filled by any object from either order_list, temp_board reverts back to og_board. 
-- The next piece from order_2 is taken, and added to new temp_board and the loop checks again.
-- If all the pieces in order_2 are used, the coordinate shifts and the loop starts again keeping the last board with cell content < 3 and missing board piece, unfilled
-- If all conditions are met and the resulting board from the loop matches the final_board, game is finished
-- The process can be visualized with the PNG-file
-'''
-
 # presets of playing piece-order, the algorithm will use to assert if its condition is met.
 piece_order_1 = [piece_blue, piece_red, piece_green, piece_pink]
 piece_order_2 = [piece_blue, piece_red, piece_green, piece_pink]
